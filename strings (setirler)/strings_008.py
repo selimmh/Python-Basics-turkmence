c388c07099fae929358b7b560bf3399e632c3cde
selim <selim.mht@gmail.com>
2020-04-18 02:26:01 -0700

    Delete strings_008.py

--- strings (setirler)/strings_008.py
+++
@@ -1,7 +0,0 @@
-#'find' metody belli bir setiriň indeks pozisiýasyny
-# tapmak üçin ulanylýar
-#indeks pozisiýasy 0 bilen başlaýar
-
-my_fruit = "Iň gowy görýän miwäm alma".find('alma')
-
-print(my_fruit)
