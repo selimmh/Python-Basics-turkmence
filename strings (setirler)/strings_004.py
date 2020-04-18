0cf9981c8c45f62751430c68ccf138b91fb64b3d
selim <selim.mht@gmail.com>
2020-04-18 02:25:32 -0700

    Delete strings_004.py

--- strings (setirler)/strings_004.py
+++
@@ -1,8 +0,0 @@
-#tapawutly variable-leri + belgisi arkaly bile ýazmak bolar
-
-first_name = "Selim"
-last_name = "Muhammet"
-
-print("Salam " + first_name)
-
-print("Gadyrdan " + first_name + " " + last_name)
