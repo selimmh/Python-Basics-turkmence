3e2fa63690748464363cf62371a0ceeb3b40a1c2
selim <selim.mht@gmail.com>
2020-04-20 21:57:13 +0300

    Add files via upload

---
+++ 7.dictionaries (sözlükler)/033_sozluklere-giris.py
@@ -0,0 +1,8 @@
+# sözlükler
+
+# açarsözler we olaryň bahalary
+cars = {'mercedes' : '10.000 $', 'tesla' : '22.000 $'}
+
+# açarsözi ulanyp elementiň bahasyny print etmek
+print("Price of the Mercedes is " + cars['mercedes'])
+print("Price of the Tesla is " + cars['tesla'])
---
+++ 7.dictionaries (sözlükler)/034_sozluklere-element-gosmak.py
@@ -0,0 +1,12 @@
+# adding item to dictionary
+
+cars = {}
+
+cars['bmw'] = '15.000$'
+cars['toyota'] = '7.000$'
+cars['opel'] = '3.000$'
+
+print("Prices are: ")
+print(cars['bmw'])
+print(cars['toyota'])
+print(cars['opel'])
