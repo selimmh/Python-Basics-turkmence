765dc56ff97f0486ea35251ce598fb2d62dde558
selim <selim.mht@gmail.com>
2020-04-22 00:21:18 -0700

    Delete 035_sozlukleri-editlemek.py

--- 7.dictionaries (sözlükler)/035_sozlukleri-editlemek.py
+++
@@ -1,7 +0,0 @@
-# sözlükdäki elementleri editlemek
-
-books = {'author' : 'Khaled Hosseini', 'name' : 'Kite runner'}
-
-print(books.get('author'))
-print(books.get('name'))
-print(books.get('publisher', 'Tapylmady'))
