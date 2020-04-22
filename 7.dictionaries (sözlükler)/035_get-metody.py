9ae2c9b37a7323279edc4002ea09f12856c79f04
selim <selim.mht@gmail.com>
2020-04-22 10:21:36 +0300

    Add files via upload

---
+++ 7.dictionaries (sözlükler)/035_get-metody.py
@@ -0,0 +1,7 @@
+# get motodyny ulanyp elementleri printlemek
+
+books = {'awtory' : 'Khaled Hosseini', 'ady' : 'Kite runner'}
+
+print(books.get('author'))
+print(books.get('name'))
+print(books.get('publisher', 'Not found.'))
