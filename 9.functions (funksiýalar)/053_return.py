e1e294e95a73dc408d1f3f474919d5167295af3a
selim <selim.mht@gmail.com>
2020-04-28 14:39:47 +0300

    Add files via upload

---
+++ 9.functions (funksiýalar)/053_return.py
@@ -0,0 +1,7 @@
+# funksiýada return ulanmak
+
+def my_function(name,surname):
+    full_name = name.title() + " " + surname.title()
+    return full_name
+
+print(my_function('selim', 'mh'))
---
+++ 9.functions (funksiýalar)/054_return2.py
@@ -0,0 +1,6 @@
+# funksiýada return ulanmak v.2
+
+def my_function1(name,surname):
+    return name.title() + " " + surname.title()
+
+print(my_function1('selim', 'mh'))
